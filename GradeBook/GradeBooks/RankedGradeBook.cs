ca34d9cdf607805e72f9dd1eb4b2cae0a1b215a8
Christopher Burch <christopher.burch2@gmail.com>
2018-09-04 15:59:21 -0500

    Completed Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,44 @@ namespace GradeBook.GradeBooks
 		{
 			Type = Enums.GradeBookType.Ranked;
 		}
+
+		public override char GetLetterGrade(double averageGrade)
+		{
+			if (this.Students.Count < 5) //Case: less than 5 students in a class
+				throw new InvalidOperationException("Ranked grading requires 5 or more students.");
+
+			//Determines 20% of students
+			double studentIncrement = (double)Students.Count / 5;
+
+			//Populate list of student grades and sort by average grades (descending)
+			List<double> gradeList = new List<double>();
+			foreach (Student s in Students)
+				gradeList.Add(s.AverageGrade);
+			gradeList.Sort();
+			gradeList.Reverse();
+
+			//Use an if-else if-else statement to determine what to return
+			if (averageGrade >= gradeList[((int)Math.Ceiling(studentIncrement))-1])
+			{
+				return 'A';
+			}
+			else if (averageGrade >= gradeList[((int)Math.Ceiling(studentIncrement * 2)) -1])
+			{
+				return 'B';
+			}
+			else if (averageGrade >= gradeList[((int)Math.Ceiling(studentIncrement * 3)) -1])
+			{
+				return 'C';
+			}
+			else if (averageGrade >= gradeList[((int)Math.Ceiling(studentIncrement * 4)) -1])
+			{
+				return 'D';
+			}
+			else
+				return 'F';
+
+
+
+		}
 	}
 }
